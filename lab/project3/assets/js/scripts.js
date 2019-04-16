6c47fd1f58f34255f8ece8f9125be846e2435723
Aira Dolfo <42970035+dolfa226@users.noreply.github.com>
2019-04-16 17:09:43 -0400

    Add files via upload

---
+++ lab/project3/assets/js/scripts.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("month");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction2() {
+    var x = document.getElementById("day");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
