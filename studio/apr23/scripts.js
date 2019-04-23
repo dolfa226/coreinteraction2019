511728bd922454ee91e432ce40b0a2fe6d7a4f60
Aira Dolfo <42970035+dolfa226@users.noreply.github.com>
2019-04-23 19:50:53 -0400

    Add files via upload

---
+++ studio/apr23/scripts.js
@@ -0,0 +1,28 @@
+// check if document is ready 
+$(document).ready(function() {
+
+var doc = $(document);
+
+var width = doc.width() - 100;
+var height = doc.height() - 100;
+
+var shapes = [ $(".circle") , $(".triangle"), $(".square") ]
+
+function randomStuff(){
+
+var randomShape = shapes[ Math.floor (Math.random() * shapes.length) ]
+var clone = randomShape.clone().appendTo('body');
+
+clone.last().css('left', Math.random()* width )
+clone.last().css('top', Math.random()* height )
+}
+
+setInterval(function(){
+    
+randomStuff();
+
+},100)
+
+
+// end document ready function
+})
